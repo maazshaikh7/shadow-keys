560a8abfae694cbace355cd92cde0882de0b6b6c
Maaz <maazshaikh4559@gmail.com>
2023-07-03 18:28:54 +0530

    Added background keyboard image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,42 +9,13 @@ type navProps = {
 export default function Navbar(props: navProps) {
   return (
     <nav
-      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
-      style={{ borderBottom: "solid 2px lightblue" }}
+      className={`navbar navbar-expand-lg navbar-dark`}
+      style={{ borderBottom: "solid 2px white" }}
     >
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-          <div className="form-check form-switch px-4">
-            <input
-              className="form-check-input"
-              onClick={props.toggleMode}
-              type="checkbox"
-              id="text-dark"
-            />
-            <label
-              className="form-check-label"
-              htmlFor="text-dark"
-              style={{ color: "gray" }}
-            >
-              Switch mode
-            </label>
-          </div>
-        </div>
       </div>
     </nav>
   );
